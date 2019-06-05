1ef171cc3aa1fbd9b98ea0542057b22208b21677
rahulpattar <rahul.pattar@gmail.com>
2019-06-05 20:34:54 +0530

    implemented sending activity

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -56,17 +56,13 @@ public class MainActivity extends AppCompatActivity {
                             Log.w("HI", "getInstanceId failed", task.getException());
                             return;
                         }
-
                         // Get new Instance ID token
                         token = task.getResult().getToken();
-
-                        // Log and toast
-                        //String msg = getString(R.string.msg_token_fmt, token);
                         Log.d("TOKEN", token);
                         Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                     }
                 });
-        myDeviceID = Build.ID;// + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Timestamp(System.currentTimeMillis()));
+        myDeviceID = Build.ID;
     }
 
     /** Called when the user taps the Send button */
@@ -88,30 +84,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://ra371996-get-quote.apps.dev.pcf-aws.com/";
-
         System.out.println("Device Model: " + myDeviceID);
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.toString());
-                        //message = "Response is: "+ response.substring(0,500);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //message = "That didn't work!";
-                textView.setText("That didn't work!");
-            }
-        });
-
         //PUT request
-
-        url = "https://iotpcf.apps.dev.pcf-aws.com/device";
+        String url = "https://iotpcf.apps.dev.pcf-aws.com/device";
         StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
                 new Response.Listener<String>()
                 {
@@ -154,12 +130,79 @@ public class MainActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queue.add(putRequest);
-        //Intent intent = new Intent(this, DisplayMessageActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        //startActivity(intent);
+   /*     Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);*/
     }
 
+    /** Called when the user taps the Send button */
+    public void sendActivity(View view) {
 
+        final TextView textView = (TextView) findViewById(R.id.textView3);
+        JSONObject jsonBodyObj = new JSONObject();
+
+        try{
+            jsonBodyObj.put("deviceID", myDeviceID);
+            jsonBodyObj.put("activity", "CPU50");
+            jsonBodyObj.put("action", "NOTIFY");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        final String requestBody = jsonBodyObj.toString();
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        //PUT request
+        String url = "https://iotpcf.apps.dev.pcf-aws.com/device/activity";
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        textView.setText("Response is: "+ response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                        textView.setText("Error.Response " + error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Accept", "application/json");
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override    public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(putRequest);
+   /*     Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);*/
+    }
 }
